e4c9e79a84d82548c6c80ee99065aa8f9ec4df8f
Hamid Zade <hamidmamadzade@gmail.com>
2016-04-01 03:18:57 +0100

    gitignore modified

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,5 +1,5 @@
 class Chitter < Sinatra::Base
-post '/sessions' do
+  post '/sessions' do
     @user = User.authenticate(params[:username], params[:password])
     if @user
       session[:user_id] = @user.id
